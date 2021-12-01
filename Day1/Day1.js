f17cd1c8f77b2505766e68f0eaa862afa42b90e4
Stanley Wong <stanley.wong@mediahuis.be>
2021-12-01 11:28:39 +0100

    day 1 part 2

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -2,13 +2,37 @@ var fs = require("fs");
 var input = fs.readFileSync("Day1Input.txt").toString().split("\n");
 
 var increase = 0;
-var checkIncrease = (prev, current) => { if (current > prev) { increase++ } }
-
-var part1 = () =>{
-    for(var i = 1;i<input.length;i++){
-        checkIncrease(i-1,i);
+var checkIncrease = (prev, current) => {
+    if (parseInt(current) > parseInt(prev)) {
+        increase++
     }
-    console.log(increase);
 }
 
-part1();
+var part1 = () => {
+    for (var i = 1; i < input.length; i++) {
+        checkIncrease(input[i - 1], input[i]);
+    }
+    console.log("Part 1 - number of increases is: " + increase);
+}
+
+part1(input);
+
+var slidingWindow = [];
+
+for (var i = 2; i < input.length; i++) {
+
+    var temp = parseInt(input[i]) + parseInt(input[i - 1]) + parseInt(input[i - 2]);
+    slidingWindow[i] = temp;
+
+
+}
+
+var part2 = () => {
+    increase = 0;
+    for (var i = 1; i < input.length; i++) {
+        checkIncrease(slidingWindow[i - 1], slidingWindow[i]);
+    }
+    console.log("Part 2 - number of increases with sliding window is: " + increase);
+}
+
+part2();
